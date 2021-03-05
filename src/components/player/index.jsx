122f9d28b09e8d2d5c1a1854bfcb5c7c17b7c41d
Tim <ryalin1286@gmail.com>
2021-03-05 19:59:52 +0000

    added walk animations

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 import Actor from "../actor";
 import useKeyPress from "../../hooks/use-key-press";
+import useWalk from "../../hooks/use-walk";
 
 export default function Player({ skin }) {
 
+    const { dir, step, walk, directions } = useWalk(3)
     const data = {
         h: 32,
         w: 32,
     };
 
-    const directions = {
-        down: 0,
-        left: 1,
-        right: 2,
-        up: 3,
-    }
-
     useKeyPress((e) => {
         const dir = e.key.replace("Arrow", "").toLowerCase()
+        walk(dir)
 
         if (directions.hasOwnProperty(dir)) {
             console.dir(dir);
@@ -26,6 +22,8 @@ export default function Player({ skin }) {
         e.preventDefault();
     })
 
-    return <Actor sprite={`/${skin}.png`} data={data} />
+
+
+    return <Actor sprite={`/${skin}.png`} data={data} step={step} dir={dir} />
 
 }
