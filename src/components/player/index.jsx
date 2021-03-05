81c4b94e3c42bd5cfd134ecb75be24227796643c
Tim <ryalin1286@gmail.com>
2021-03-05 09:31:39 +0000

    added keybinding hook for directional movement

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Actor from "../actor";
-
+import useKeyPress from "../../hooks/use-key-press";
 
 export default function Player({ skin }) {
 
@@ -8,6 +8,23 @@ export default function Player({ skin }) {
         h: 32,
         w: 32,
     };
+
+    const directions = {
+        down: 0,
+        left: 1,
+        right: 2,
+        up: 3,
+    }
+
+    useKeyPress((e) => {
+        const dir = e.key.replace("Arrow", "").toLowerCase()
+
+        if (directions.hasOwnProperty(dir)) {
+            console.dir(dir);
+        }
+
+        e.preventDefault();
+    })
 
     return <Actor sprite={`/${skin}.png`} data={data} />
 
