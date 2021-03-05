4eacc24cceded15cfad7b614147ba2cdcdb56ef4
Tim <ryalin1286@gmail.com>
2021-03-05 20:32:24 +0000

    added character movement

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -5,7 +5,7 @@ import useWalk from "../../hooks/use-walk";
 
 export default function Player({ skin }) {
 
-    const { dir, step, walk, directions } = useWalk(3)
+    const { dir, step, walk, position } = useWalk(3)
     const data = {
         h: 32,
         w: 32,
@@ -15,15 +15,19 @@ export default function Player({ skin }) {
         const dir = e.key.replace("Arrow", "").toLowerCase()
         walk(dir)
 
-        if (directions.hasOwnProperty(dir)) {
-            console.dir(dir);
-        }
-
         e.preventDefault();
     })
 
 
 
-    return <Actor sprite={`/${skin}.png`} data={data} step={step} dir={dir} />
+    return (
+        <Actor
+            sprite={`/${skin}.png`}
+            data={data}
+            step={step}
+            dir={dir}
+            position={position}
+        />
+    )
 
 }
